a3fd1819e8f33c1292d38f967e4913bdc59e1b3d
Fernanda Diniz <dinizfernanda736@gmail.com>
2023-05-04 16:55:24 -0300

    Adicionar arquivos de projeto.

---
+++ ExecicioEnumComposition/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecicioEnumComposition.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        Peding_Payment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
---
+++ ExecicioEnumComposition/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExecicioEnumComposition.Entities;
+using ExecicioEnumComposition.Entities.Enums;
+using System.Globalization;
+
+Console.WriteLine("Enter cliente data: ");
+Console.Write("Name: ");
+string name = Console.ReadLine();
+Console.Write("Email: ");
+string email = Console.ReadLine();
+Console.Write("Birth date (DD/MM/YYYY): ");
+DateTime birth = DateTime.Parse(Console.ReadLine());
+Console.WriteLine("Enter order data: ");
+Console.Write("Status: ");
+OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+Client client = new Client(name, email, birth);
+Order order = new Order(DateTime.Now, status, client);
+
+Console.Write("How many items to this order?");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.WriteLine($"Enter #{i} item data: ");
+    Console.Write("Product name: ");
+    string prodName = Console.ReadLine();
+    Console.Write("Product price: ");
+    double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    
+    Product product = new Product(prodName, price);
+
+    Console.Write("Quantity: ");
+    int quantity = int.Parse(Console.ReadLine());
+    
+    OrderItem orderItem = new OrderItem(quantity,price, product);
+
+    order.AddItem(orderItem);
+}
+
+Console.WriteLine();
+Console.WriteLine("ORDER SUMMARY: ");
+Console.WriteLine(order);
